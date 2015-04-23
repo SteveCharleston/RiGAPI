b5490da0958f5d6568d5f9d913829796c7865672
SteveCharleston <schalhorn@gmail.com>
2015-04-23 16:13:16 +0200

    Added first file

---
+++ RiG-Lib/src/rigAPI/RigDBAccess.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package rigAPI;
+
+/**
+ * @author steven
+ *
+ */
+public class RigDBAccess {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
