5f9893098cf2c060173af01ff513066fc7545ec9
SteveCharleston <schalhorn@gmail.com>
2015-04-23 22:49:59 +0200

    Erste Anpassungen

--- RiG-Lib/src/rigAPI/RigDBAccess.java
+++ RiG-Lib/src/rigAPI/RigDBAccess.java
@@ -3,18 +3,33 @@
  */
 package rigAPI;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
 /**
  * @author steven
  *
  */
 public class RigDBAccess {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    private final String USER_AGENT = "Mozilla/5.0";
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-	}
+    }
+
+    public static void httpPost(String url, List<NameValuePair> urlParameters)
+        throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // request header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+    }
 
 }
