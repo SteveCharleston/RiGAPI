7f4e2ef89b62aaa8a02291286c146c25d196aa6e
SteveCharleston <schalhorn@gmail.com>
2015-04-26 23:53:35 +0200

    Erster Checkin - httppost api - login code

--- RiG-Lib/src/rigAPI/RigDBAccess.java
+++ RiG-Lib/src/rigAPI/RigDBAccess.java
@@ -3,8 +3,20 @@
  */
 package rigAPI;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URLEncoder;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,23 +25,79 @@ import java.util.List;
  */
 public class RigDBAccess {
 
-    private final String USER_AGENT = "Mozilla/5.0";
+    private String API_KEY;
+    private static String USER_AGENT = "Mozilla/5.0";
+    private static String APIURL
+            = "http://bewerbung.rockimgruenen.de/api/";
     /**
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+
+        List<NameValuePair> loginParams = new ArrayList<NameValuePair>(2);
+        loginParams.add(new BasicNameValuePair("user", "user2"));
+        //loginParams.add(new BasicNameValuePair("password",
+                //"7c6a180b36896a0a8c02787eeafb0e4c"));
+        loginParams.add(new BasicNameValuePair("password",
+                "6cb75f652a9b52798eb6cf2201057c73"));
+
+        try {
+            String loginRet = httpPost(url, loginParams);
+            System.out.println(loginRet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public static void httpPost(String url, List<NameValuePair> urlParameters)
-        throws Exception {
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+    public void authenticate(List<NameValuePair> params) throws
+            Exception, NoUserException {
+        String pageURL = APIURL + "authenticate.php";
 
-        // request header
-        con.setRequestMethod("POST");
-        con.setRequestProperty("User-Agent", USER_AGENT);
+        String result = httpPost(pageURL, params);
+
+        if ("NO_USER".equals(result)) {
+            throw new NoUserException();
+        } else if ("NO_PASSWORD".equals(result)) {
+            throw new NoPasswordException();
+        } else if ("BAD_AUTHENTICATON".equals(result)) {
+            throw new BadAuthenticationException();
+        } else if ("BROKEN_APIKEY".equals(result)) {
+            throw new BrokenAPIKeyException();
+        }
+
+        this.API_KEY = result;
     }
 
+    public static String httpPost(String url, List<NameValuePair> urlParameters)
+            throws Exception {
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(url);
+
+        post.setHeader("User-Agent", USER_AGENT);
+        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+        HttpResponse response = client.execute(post);
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader
+                (response.getEntity().getContent()));
+
+        StringBuffer result = new StringBuffer();
+        String line = "";
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+
+        return result.toString();
+    }
+
+    private static class NoUserException extends Exception { }
+
+    private static class NoPasswordException extends Exception { }
+
+    private static class BadAuthenticationException extends Exception { }
+
+    private static class BrokenAPIKeyException extends Exception { }
 }
+
+
